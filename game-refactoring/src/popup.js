6c2f5ead13f80ec849a7ae66711b485a7e99dbde
seanwuvely <seanwuvely@gmail.com>
2021-03-31 09:44:31 +0900

    add carrot-game

---
+++ game-refactoring/src/popup.js
@@ -0,0 +1,27 @@
+"use strict";
+
+export default class PopUp {
+  constructor() {
+    this.popUp = document.querySelector(".pop-up");
+    this.popUpText = document.querySelector(".pop-up__message");
+    this.popUpRedo = document.querySelector(".pop-up__redo");
+
+    this.popUpRedo.addEventListener("click", () => {
+      this.onClick && this.onClick(); // startGame();
+      this.hide();
+    });
+  }
+
+  setClickListener(onClick) {
+    this.onClick = onClick;
+  }
+
+  showWithText(text) {
+    this.popUpText.innerText = text;
+    this.popUp.classList.remove("pop-up--hide");
+  }
+
+  hide() {
+    this.popUp.classList.add("pop-up--hide");
+  }
+}
